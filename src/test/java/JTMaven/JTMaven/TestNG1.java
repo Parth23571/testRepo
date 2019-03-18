a2b98e636cd8c18c23342c6c7ad4ca9254c85f39
parth235 <pawal.pankaj@gmail.com>
2019-03-18 15:59:33 +0530

    222

--- src/test/java/JTMaven/JTMaven/TestNG1.java
+++ src/test/java/JTMaven/JTMaven/TestNG1.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class TestNG1 {
   @Test
   public void f() {
-	  System.out.println("In TestNG1 Dated on 116 March 18 March-21");
+	  System.out.println("In TestNG1 Dated on 116 March 18 March-211");
   }
 }
 	
