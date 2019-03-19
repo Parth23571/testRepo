fd9631b58f16c1c08f41891fd249a524907f3443
parth235 <pawal.pankaj@gmail.com>
2019-03-19 15:05:22 +0530

    dd

--- src/test/java/JTMaven/JTMaven/TestNG1.java
+++ src/test/java/JTMaven/JTMaven/TestNG1.java
@@ -6,8 +6,7 @@ public class TestNG1 {
   @Test
   public void f() {
 
-	  System.out.println("In TestNG1 Dated on 1 16 March 1 18 March-1-11k-qk");
-	  System.out.println("In TestNG1 Dated on 1 16 March 1 18 March-1-pk");
+	  System.out.println("In TestNG1 Dated on 1 16 March 1 18 March-1-11k-qk-dd");
 
 	  }
 
